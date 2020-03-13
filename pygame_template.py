fe0e01456585d7ff50bf00c9c6ca5a9d6569df9c
Sergey Lebedev <lebedev2k@mail.ru>
2020-03-13 09:03:37 +0700

    initial commit

---
+++ pygame_template.py
@@ -0,0 +1,20 @@
+import pygame
+from sys import exit
+
+pygame.init()
+win = pygame.display.set_mode((1050, 500))
+pygame.display.set_caption('Pygame template')
+clock = pygame.time.Clock()
+
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    win.fill((100,255,0))
+
+    pygame.display.update()
+    clock.tick(30)
+
