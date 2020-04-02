0c6feb0a51e3b80b208bb60b1f90a8e792df765a
Lebedev Sergey <lebedev2k@mail.ru>
2020-04-02 13:55:46 +0700

    Добавили картинки в папку coins для размещения на поле анимированных монеток

--- platformer/test.py
+++ platformer/test.py
@@ -1,28 +1,9 @@
-import pygame
-import pyganim
+import os
+import sys
 
-pygame.init()
+dir = os.path.dirname(__file__) + '/img'
 
-# a = [
-#     ('.\walk001.png', 100),
+for f in os.listdir(dir+'/coins'):
+    print(f + ': ' + str(os.path.isfile(dir + '/' + f)))
 
-# ]
-# anim = pyganim.PygAnimation(a)
-# anim.play()
-
-win = pygame.display.set_mode((640, 320))
-pygame.display.set_caption('Platformer')
-clock = pygame.time.Clock()
-
-bk = pygame.image.load('platformer/img/2_enemies_1_walk_000.png')
-s = pygame.Surface((300, 300))
-s.blit(bk, (0, 0))
-
-run = True
-while run:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            run = False
-    win.blit(bk, (0, 0))
-    pygame.display.update()
-    clock.tick(30)
+# print(dir)
