8438eedfc5e0b186014ba8f8ae84ef1f422059c2
Sergey Lebedev <lebedev2k@mail.ru>
2020-04-03 14:03:15 +0700

    Новая вспом.фукнция getImagesFromSpriteSheet для загрузки картинок

--- platformer/utils.py
+++ platformer/utils.py
@@ -1,5 +1,6 @@
 import os
-
+import pyganim
+import pygame
 
 def getPygAnimListFromDir(dirpath, delay):
     res = []
@@ -11,8 +12,23 @@ def getPygAnimListFromDir(dirpath, delay):
         # print(s)
 
     return res
-
+def getImagesFromSpriteSheet(filename, startX, startY, width, height, rows, cols):
+    x = startX
+    y = startY
+    res = []
+    for i in range(0, rows):
+        for j in range(0, cols):
+            rect = (x,y,width,height)
+            res.append(rect)
+            x += width
+        x = startX
+        y += height
+    
+    return pyganim.getImagesFromSpriteSheet(filename, rects=res)
 
 # path = os.path.dirname(__file__)+'/img/coins/1'
 # animList = getPygAnimListFromDir(path, 100)
 # print(animList)
+if __name__ == "__main__":
+    a = getImagesFromSpriteSheet('platformer/img/character/run/1x.png',0,0,100,100,1,10)
+    print(a)
